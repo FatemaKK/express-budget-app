ac7d3fdcd8f2e12b0b05aacc046459068f1317a0
Fatema Kabir <fatema.kabir@pursuit.org>
2022-01-25 13:57:18 -0500

    object update

--- models/transaction.js
+++ models/transaction.js
@@ -3,56 +3,78 @@ const transaction = [
     date: "April 1",
     source: "Income",
     amount: 1000,
+    from: "Outsource",
+    category: "Income"
   },
   {
     date: "April 1",
     source: "Taxes",
     amount: -240,
+    from: "IRS",
+    category: "Civic Duty"
   },
   {
     date: "April 1",
     source: "Security Fund",
     amount: -150,
+    from: "401K",
+    category: "Savings",
   },
   {
     date: "April 1",
     source: "Savings",
     amount: -100,
+    from: "Wells Fargo",
+    category: "Savings",
   },
   {
     date: "April 1",
     source: "CC Payment",
     amount: -80,
+    from: "Capital One",
+    category: "Splurge"
   },
   {
     date: "April 3",
     source: "Birthday $$$",
     amount: 160,
+    from: "Residual",
+    category: "Blessing"
   },
   {
     date: "April 3",
-    source: "Spirits/Caffeine",
+    source: "Spirits",
     amount: -14,
+    from: "Bodega",
+    category: "Recreational"
   },
   {
     date: "April 3",
     source: "WiFi",
     amount: -80,
+    from: "Fios",
+    category: "Bare Necessity"
   },
   {
     date: "April 5",
     source: "Grocery",
     amount: -76,
+    from: "Trader Joe's",
+    category: "Bare Necessity"
   },
   {
     date: "April 6",
     source: "Dine-Out",
     amount: -180,
+    from: "Rangoon",
+    category: "Splurge"
   },
   {
     date: "April 6",
     source: "Utility",
-    amount: -220,
+    amount: -30,
+    from: "Mint",
+    category: "Bare Necessity"
   },
 ];
 
