2b0db0dff11e5db4414a55963b7f1660e13970a0
Fatema Kabir <fatema.kabir@pursuit.org>
2022-01-25 22:33:19 -0500

    object

--- models/transaction.js
+++ models/transaction.js
@@ -1,76 +1,76 @@
 const transaction = [
   {
-    date: "April 1",
+    date: "January 12 2022",
     source: "Income",
     amount: 1000,
     from: "Outsource",
     category: "Income"
   },
   {
-    date: "April 1",
+    date: "January 14 2022",
     source: "Taxes",
     amount: -240,
     from: "IRS",
     category: "Civic Duty"
   },
   {
-    date: "April 1",
+    date: "January 15 2022",
     source: "Security Fund",
     amount: -150,
     from: "401K",
     category: "Savings",
   },
   {
-    date: "April 1",
+    date: "January 15 2022",
     source: "Savings",
     amount: -100,
     from: "Wells Fargo",
     category: "Savings",
   },
   {
-    date: "April 1",
+    date: "January 16 2022",
     source: "CC Payment",
     amount: -80,
     from: "Capital One",
     category: "Splurge"
   },
   {
-    date: "April 3",
+    date: "January 18 2022",
     source: "Birthday $$$",
     amount: 160,
     from: "Residual",
     category: "Blessing"
   },
   {
-    date: "April 3",
+    date: "January 18 2022",
     source: "Spirits",
     amount: -14,
     from: "Bodega",
     category: "Recreational"
   },
   {
-    date: "April 3",
+    date: "January 20 2022",
     source: "WiFi",
     amount: -80,
     from: "Fios",
     category: "Bare Necessity"
   },
   {
-    date: "April 5",
+    date: "January 20 2022",
     source: "Grocery",
     amount: -76,
     from: "Trader Joe's",
     category: "Bare Necessity"
   },
   {
-    date: "April 6",
+    date: "January 22 2022",
     source: "Dine-Out",
     amount: -180,
     from: "Rangoon",
     category: "Splurge"
   },
   {
-    date: "April 6",
+    date: "January 22 2022",
     source: "Utility",
     amount: -30,
     from: "Mint",
